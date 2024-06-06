87d34786b9f6e0a8f9087c467759221c01e62e83
juandz13 <jdiaz@ximdex.com>
2024-06-06 12:10:17 +0200

    role admin superadmin applied to get user

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserPaginationRequest;
 use App\Services\UserService;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
 
     protected $userService;
+    protected $auth;
 
-    public function __construct(UserService $userService)
+    public function __construct(UserService $userService,Guard $auth)
     {
         $this->userService = $userService;
+        $this->auth = $auth;
     }
 
     public function listUsers(UserPaginationRequest $request)
@@ -33,14 +36,20 @@ class UserController extends Controller
 
     public function getUser(string $id)
     {
-        try {
-            $user = $this->userService->getUserById($id);
-            $user->makeHidden(['password', 'remember_token','email_verified_at','created_at','updated_at','roles']); 
-            return response()->json(['user' => $user]);
-        } catch (\Exception $e) {
-            \Log::error($e->getMessage());
-            return response()->json(['error' => 'An error occurred while fetching the user.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+   
+        if ($this->auth->user()->hasRole(['admin','superadmin'])){
+            try {
+                $user = $this->userService->getUserById($id);
+                $user->makeHidden(['password', 'remember_token','email_verified_at','created_at','updated_at','roles']); 
+                return response()->json(['user' => $user]);
+            } catch (\Exception $e) {
+                \Log::error($e->getMessage());
+                return response()->json(['error' => 'An error occurred while fetching the user.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }else{
+            return response()->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
         }
+        
     }
 
     public function deleteUser($id)
