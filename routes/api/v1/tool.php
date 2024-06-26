9b2d69aa950734cb030a8ab5871e8bacfbae08f7
juandz13 <jdiaz@ximdex.com>
2024-06-26 08:29:59 +0200

    services routes

--- routes/api/v1/tool.php
+++ routes/api/v1/tool.php
@@ -1,11 +1,12 @@
 <?php
 
-// Role Routes
-
 use App\Http\Controllers\Tools\ToolController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('services')->middleware('auth:api')->group(function () {
     Route::get('/', [ToolController::class, 'getList'])->name('api.tools.getList');
+});
+
+Route::prefix('services')->middleware(['auth:api','role:admin|superadmin'])->group(function () {
     Route::get('/create-user-on-service/{user}/{serviceId}', [ToolController::class, 'createUserOnService'])->name('api.tools.createUserOnService');
 });
